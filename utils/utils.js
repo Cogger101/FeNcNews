2d2066d8aca5a3eca8ff6a12728064c28b9a17b3
Cogger101 <seandcoughlan@hotmail.co.uk>
2024-09-02 18:06:19 +0100

    changed base URL and get request paths

--- utils/utils.js
+++ utils/utils.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
 const ncNewsApiMain = axios.create({
-    baseURL: `https://nc-news-tthj.onrender.com/`
+    baseURL: `https://nc-news-tthj.onrender.com`
 })
 
 export function getAllArticles(){
-    return ncNewsApiMain.get(`api/articles`)
+    return ncNewsApiMain.get(`/api/articles`)
     }
 
 export function getArticleById(article_id){
-    return ncNewsApiMain.get(`api/articles/${article_id}`)
+    return ncNewsApiMain.get(`/api/articles/${article_id}`)
     }
 
 export function getArticleComments(article_id){
@@ -17,5 +17,5 @@ export function getArticleComments(article_id){
 }
 
 export function patchVotes(article_id, updatedVotes){
-    return ncNewsApiMain.patch(`api/articles/${article_id}`, updatedVotes)
+    return ncNewsApiMain.patch(`/api/articles/${article_id}`, updatedVotes)
 }
